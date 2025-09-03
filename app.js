6142c38a91e119be35005d0667357e4fa02ee364
VITOR MIGUEL RODRIGUES CEZARIO <25132565@CFP127ALUNO.LOCAL>
2025-09-03 13:25:07 -0300

    Modificado app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import produtos from './produtos_atualizados.json' with {type: 'json'}
 
-function criarFuncionario(dados) {
+function criarProduto(dados) {
 
     const container = document.getElementById('produto_oferta')
     const produto = document.createElement('content_produto_oferta')
@@ -12,21 +12,32 @@ function criarFuncionario(dados) {
     const li_preco = document.createElement('li')
     const descricao = document.createElement('a')
     const li_descricao = document.createElement('li')
+    const id_produto = document.createElement('h1')
+    const classificacao = document.createElement('h1')
     const pastaimg = "./img/"
 
-    container.classList.add('produto_oferta');
     produto.classList.add('content_produto_oferta');
-    
     imagem.src = pastaimg + dados.imagem
     imagem.alt = dados.nome
     nome.textContent = dados.nome
-    nome.classList.add('nome')
-    cargo.textContent = dados.cargo
-    cargo.classList.add('cargo')
+    categoria.textContent = dados.categoria
+    preco.textContent = dados.preco
+    descricao.textContent = dados.descricao
     
 
-    container.appendChild(funcionario)
-    funcionario.appendChild(imagem)
-    funcionario.appendChild(nome)
-    funcionario.appendChild(cargo)
+    container.appendChild(produto)
+    produto.appendChild(categoria)
+    produto.appendChild(imagem)
+    produto.appendChild(li_nome)
+    li_nome.appendChild(nome)
+    produto.appendChild(preco)
+    preco.appendChild(li_preco)
+    produto.appendChild(li_descricao)
+    li_descricao.appendChild(descricao)
 }
+
+function carregarProduto() {
+    produtos.forEach(criarProduto)
+}
+
+carregarProduto()
